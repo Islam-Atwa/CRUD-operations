6197efc60bd69246a979cb42d04b1440b8f4934f
Islam Atwa <islamatwa429@gmail.com.com>
2025-08-10 03:41:57 +0300

    compolete Feature Create

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,11 +1,11 @@
 
 import { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import array from "./array";
+
 import { Link , useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2';
-
-
-
+import { v4 as uuidv4 } from 'uuid';
 
 import { Button, Form } from 'react-bootstrap';
 
@@ -15,8 +15,17 @@ function Create(){
     const [name, setName] = useState('');
     const [age, setAge] = useState('');
 
+    let history = useNavigate();
+
+
+
     const handelSubmit = (e)=>{
         e.preventDefault();
+
+        const ids = uuidv4();
+        let uni = ids.slice(0, 8);
+
+        let a = name, b = age;
 
         if(name === '' || age === ''){
             Swal.fire({
@@ -26,6 +35,10 @@ function Create(){
             });
             return;
         }
+        array.push({ id: uni, Name: a, Age: b });
+        history("/");
+
+
     }
 
   
@@ -40,8 +53,11 @@ function Create(){
                     controlId="formBasicName"
                 >
                     <Form.Control
+                        onChange={(e)=>
+                            setName(e.target.value)
+                        }
                         type="text"
-                        placeholder="Enter Name"
+                        placeholder="Enter Name "
                         required
                     />
                 </Form.Group>
@@ -51,6 +67,9 @@ function Create(){
                     controlId="formBasicName"
                 >
                     <Form.Control
+                        onChange={(e)=>
+                            setAge(e.target.value)
+                        }
                         type="number"
                         placeholder="Age"
                         required
@@ -58,7 +77,8 @@ function Create(){
                 </Form.Group>
 
                 <Button
-                    onClick={handelSubmit}
+                    onClick={(e) => handelSubmit(e)}
+
                     variant="primary"
                     type="submit"
                 >
