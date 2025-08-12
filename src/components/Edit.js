fa5928e9538675bcbdfe39ccf562efc9a79e1854
l <l@ls-MacBook-Air.local>
2025-08-13 01:10:21 +0300

    feat: create Edit component with Bootstrap form for name and age update

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,9 +4,51 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Edit(){
     return (
-        <>
+        <div>
+            <Form
+                className="d-grid gap-2"
+                style={{ margin: "5rem" }}
+            >
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicEmail"
+                >
+                    <Form.Control
+                        
+                        type="text"
+                        placeholder="Enter Name"
+                    />
 
-        </>
+                </Form.Group>
+
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicPassword"
+                >
+                    <Form.Control
+                        
+                        type="number"
+                        placeholder="Age"
+                    />
+                </Form.Group>
+
+                <Button
+                    variant="primary"
+                    type="submit"
+                    size="lg"
+                >
+                    Update
+                </Button>
+
+                <Link className="d-grid gap-2" to="/">
+                    <Button variant="warning" size="lg">
+                        Home
+                    </Button>
+                </Link>
+
+            </Form>
+        </div>
+        
     )
 };
 
